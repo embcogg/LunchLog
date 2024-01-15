b63d76408d7cad952a87831c8a7c4ad98dffef8b
Emily <embcogg@gmail.com>
2024-01-14 20:40:34 -0700

    Final commit

---
+++ Lunch_Log/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lunch_Log
+//
+//  Created by Emily Coggins on 1/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
